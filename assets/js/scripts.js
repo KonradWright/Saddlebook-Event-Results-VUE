84e01aeaa0c37fbb2bb84bd23e42fad37f82b9c7
Konrad Robert Wright <OKonrad95@Gmail.com>
2017-11-13 14:18:52 -0800

    Uploaded real data

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,13 +7,27 @@ new Vue({
       var filterLowercase = filter.toLowerCase();
       var found = false;
       if(filter == ''){return true;}
-      if(result.course.toLowerCase().includes(filterLowercase)){return true;}
-      else if(result.entry.toLowerCase().includes(filterLowercase)){return true;}
-      else if(result.horse.toLowerCase().includes(filterLowercase)){return true;}
-      else if(result.rider.toLowerCase().includes(filterLowercase)){return true;}
-      else if(result.trainer.toLowerCase().includes(filterLowercase)){return true;}
-      else if(result.owner.toLowerCase().includes(filterLowercase)){return true;}
+      if(result.code.toLowerCase().includes(filterLowercase)){return true;}
+      else if(result.entry_no.toLowerCase().includes(filterLowercase)){return true;}
+      else if(result.horse_name.toLowerCase().includes(filterLowercase)){return true;}
+      else if(result.rider_name.toLowerCase().includes(filterLowercase)){return true;}
+      else if(result.trainer_name.toLowerCase().includes(filterLowercase)){return true;}
+      else if(result.owner_name.toLowerCase().includes(filterLowercase)){return true;}
       return false;
+    },
+    toOrdinal: function (i) {
+        var j = i % 10,
+            k = i % 100;
+        if (j == 1 && k != 11) {
+            return i + "st";
+        }
+        if (j == 2 && k != 12) {
+            return i + "nd";
+        }
+        if (j == 3 && k != 13) {
+            return i + "rd";
+        }
+        return i + "th";
     }
   }
 });
